0246eceb58fedbefa4622702117b35f638ab99cf
paezdev <jean_paez@hotmail.es>
2025-03-05 14:05:34 -0500

    Actualización del archivo setup.py para incluir las nuevas dependencias

--- setup.py
+++ setup.py
@@ -8,12 +8,15 @@ setup(
         "pandas>=2.0.0",
         "kagglehub[pandas-datasets]>=0.3.8",
         "openpyxl>=3.1.2"
+        "matplotlib>=3.5.0",
+        "seaborn>=0.11.2",
+        "numpy>=1.20.0"
     ],
     author="Jean Carlos Páez Ramírez y Juliana Maria Peña Suarez",
     author_email="",
     asignatura="Infraestructura y arquitectura para Big Data",
-    tema="Ingestión de Datos desde un API",    
-    description="Etapa de ingesta de datos para el proyecto integrador de Big Data",
+    tema="Ingestión y procesamiento de Datos",    
+    description="Etapas de ingesta y preprocesamiento de datos para el proyecto integrador de infraestructura y arquitectura para Big Data",
     docente="Andres Felipe Callejas Jaramillo",
     python_requires=">=3.9",
 )
